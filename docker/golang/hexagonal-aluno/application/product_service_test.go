f6a9e2f99b612eebe41ba4d9d2711b3a3b99d283
Eduardo Goncalves <edugsdf@gmail.com>
2021-04-06 15:27:06 -0300

    Finalizado o projeto arquitetura hexagonal

--- docker/golang/hexagonal-aluno/application/product_service_test.go
+++ docker/golang/hexagonal-aluno/application/product_service_test.go
@@ -3,8 +3,10 @@ package application_test
 import (
 	"testing"
 
-	mock_application "github.com/edugsdf/full-cycle2-0/tree/main/docker/golang/hexagonal-aluno/mocks"
+	"github.com/edugsdf/full-cycle2-0/tree/main/docker/golang/hexagonal-aluno/application"
+	mock_application "github.com/edugsdf/full-cycle2-0/tree/main/docker/golang/hexagonal-aluno/application/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProductService_Get(t *testing.T) {
@@ -14,6 +16,56 @@ func TestProductService_Get(t *testing.T) {
 
 	product := mock_application.NewMockProductInterface(ctrl)
 	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
-	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTime()
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("1234")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+	result, err := service.Create("Product 01", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
+
+func TestProductService_EnableDisable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil)
+	product.EXPECT().Disable().Return(nil)
+
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
 
 }
